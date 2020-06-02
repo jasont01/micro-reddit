d5bd144fe6b713ac247b1f7377caa7a6ba9fd7c2
Jason Thompson <jason@jasont.us>
2020-06-01 22:36:03 -0400

    add user and post models

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :link, presence: true
+  
+  belongs_to :user
+  has_many :comments
+end
